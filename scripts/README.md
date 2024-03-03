2838ca2a2ddadbf39e29cf4c87d9d2b01ce82ea9
Everton Juniti Ogura <everton@evertonogura.com>
2024-03-03 10:24:40 -0300

    Adicionando instruções para subida de containers de banco de dados no Docker

--- scripts/README.md
+++ scripts/README.md
@@ -1,2 +1,40 @@
-# orm-performance-estudo
-Neste repo você encontra um breve estudo do impacto na performance ao usar ORMs
+# scripts
+Nesta pasta você encontra o necessário para subir um banco de dados SQL Server, MySQL e PostgreSQL usando Docker, além de dados de exemplo
+
+## Pré-requisitos
+
+* Docker instalado
+* DBeaver para rodar os scripts e comandos
+
+## Criação dos containers
+
+Utilize os comandos abaixo para criar os containers no Docker para os bancos de dados:
+
+### SQL Server
+
+`docker run --name SQLServer -p 1433:1433 --hostname sql1 -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Minha@senha" -d mcr.microsoft.com/mssql/server:latest`
+
+A porta configurada é a default para o SQL Server: 1433. Você pode mudar caso veja necessidade.
+
+### MySQL
+
+`docker run --name MySQL -p 3306:3306 -e MYSQL_ROOT_PASSWORD=Minha@senha -d mysql:latest`
+
+A porta configurada é a default para o MySQL: 3306. Você pode mudar caso veja necessidade.
+
+### PostgreSQL
+
+`docker run --name PostgreSQL -p 5432:5432 -e POSTGRES_PASSWORD=Minha@senha -d postgres`
+
+A porta configurada é a default para o PostgreSQL: 5432. Você pode mudar caso veja necessidade.
+
+### DBeaver
+
+Configure as conexões para os três bancos de dados. A vantagem do DBeaver é essa, você consegue utilizar vários bancos de dados distintos.
+
+Abaixo um exemplo de como configurar:
+
+1) Clique no botão de "Nova conexão" (destacado em vermelho)
+
+[/imagens/imagem1.png](/imagens/imagem1.png)
+
---
+++ scripts/SQLServer_Script.sql
@@ -0,0 +1,262 @@
+-- ***************************************************************
+-- ***************************************************************
+-- ***************************************************************
+-- ***************************************************************
+
+SET DATEFORMAT ymd
+
+IF EXISTS(SELECT * FROM sys.procedures WHERE name = 'PopularVendas')  
+BEGIN 
+	DROP PROCEDURE dbo.PopularVendas 
+END 
+
+IF EXISTS(SELECT * FROM sys.tables WHERE name = 'Vendas')  
+BEGIN 
+	DROP TABLE dbo.Vendas 
+END 
+
+IF EXISTS(SELECT * FROM sys.tables WHERE name = 'Produto')  
+BEGIN 
+	DROP TABLE dbo.Produto 
+END 
+
+IF EXISTS(SELECT * FROM sys.tables WHERE name = 'CadastroCliente')  
+BEGIN 
+	DROP TABLE dbo.CadastroCliente 
+END 
+
+IF EXISTS(SELECT * FROM sys.tables WHERE name = 'Cidade')  
+BEGIN 
+	DROP TABLE dbo.Cidade 
+END 
+
+IF EXISTS(SELECT * FROM sys.tables WHERE name = 'Estado')  
+BEGIN 
+	DROP TABLE dbo.Estado 
+END 
+
+GO 
+
+/*
+Tabela de dominio que representa os estados brasileiros
+*/
+
+CREATE TABLE dbo.Estado 
+(
+	Id TINYINT IDENTITY(1, 1) NOT NULL, 
+	Descricao VARCHAR(150) NOT NULL, 
+	CONSTRAINT PK_Estado PRIMARY KEY (Id) 
+)
+
+INSERT INTO dbo.Estado (Descricao) 
+VALUES ('Sao Paulo'), 
+       ('Rio de Janeiro'), 
+	   ('Minas Gerais') 
+
+/*
+Tabela de dominio que representa as cidades brasileiras
+Utiliza-se o codigo da tabela de domonio de Estado para identificar a qual estado pertence cada cidade
+*/
+
+CREATE TABLE dbo.Cidade 
+(
+	Id SMALLINT IDENTITY(1, 1) NOT NULL, 
+	Id_Estado TINYINT NOT NULL, 
+	Descricao VARCHAR(250) NOT NULL, 
+	CONSTRAINT PK_Cidade PRIMARY KEY (Id), 
+	CONSTRAINT FK_Estado_Cidade FOREIGN KEY (Id_Estado) REFERENCES Estado (Id) 
+) 
+
+INSERT INTO dbo.Cidade (Id_Estado, Descricao) 
+VALUES (1, 'Santo Andre'), 
+       (1, 'Sao Bernardo do Campo'), 
+	   (1, 'Sao Caetano do Sul'), 
+	   (2, 'Duque de Caxias'), 
+	   (2, 'Niteroi'), 
+	   (2, 'Petropolis'), 
+	   (3, 'Uberlandia'), 
+	   (3, 'Contagem'), 
+	   (3, 'Juiz de Fora') 
+
+/*
+Representacao da tabela de cadastro de clientes
+Ha vinculo do cliente com a tabela de dominio Cidade
+Como a tabela de dominio Cidade ja possui vinculo com a tabela Estado, nao e necessario criar vinculo forte entre a tabela CadastroCliente e a tabela Estado
+*/
+
+CREATE TABLE dbo.CadastroCliente 
+(
+	Id INTEGER IDENTITY(1, 1) NOT NULL, 
+	Nome VARCHAR(150) NOT NULL, 
+	Endereco VARCHAR(250) NOT NULL, 
+	Id_Cidade SMALLINT NOT NULL, 
+	Email VARCHAR(250) NOT NULL, 
+	Telefone1 VARCHAR(20) NOT NULL, 
+	Telefone2 VARCHAR(20) NULL, 
+	Telefone3 VARCHAR(20) NULL, 
+	CONSTRAINT PK_CadastroCliente PRIMARY KEY (Id), 
+	CONSTRAINT FK_Cidade_CadastroCliente FOREIGN KEY (Id_Cidade) REFERENCES Cidade (Id) 
+) 
+
+INSERT INTO dbo.CadastroCliente (Nome, Endereco, Id_Cidade, Email, Telefone1, Telefone2, Telefone3) 
+VALUES ('Cliente 1',  'Rua 1',  1, 'cliente_1@email.com',  '(11) 0000-0000', NULL, NULL), 
+       ('Cliente 2',  'Rua 2',  1, 'cliente_2@email.com',  '(11) 0000-0000', '(11) 1111-1111', '(11) 2222-2222'), 
+	   ('Cliente 3',  'Rua 3',  1, 'cliente_3@email.com',  '(11) 0000-0000', '(11) 1111-1111', '(11) 2222-2222'), 
+	   ('Cliente 4',  'Rua 4',  2, 'cliente_4@email.com',  '(11) 0000-0000', '(11) 1111-1111', NULL), 
+	   ('Cliente 5',  'Rua 5',  2, 'cliente_5@email.com',  '(11) 0000-0000', '(11) 1111-1111', '(11) 2222-2222'), 
+	   ('Cliente 6',  'Rua 6',  2, 'cliente_6@email.com',  '(11) 0000-0000', '(11) 1111-1111', NULL), 
+	   ('Cliente 7',  'Rua 7',  3, 'cliente_7@email.com',  '(11) 0000-0000', NULL,             NULL), 
+	   ('Cliente 8',  'Rua 8',  3, 'cliente_8@email.com',  '(11) 0000-0000', '(11) 1111-1111', '(11) 2222-2222'), 
+	   ('Cliente 9',  'Rua 9',  3, 'cliente_9@email.com',  '(11) 0000-0000', '(11) 1111-1111', '(11) 2222-2222'), 
+	   ('Cliente 10', 'Rua 10', 4, 'cliente_10@email.com', '(21) 0000-0000', '(21) 1111-1111', '(21) 2222-2222'), 
+	   ('Cliente 11', 'Rua 11', 4, 'cliente_11@email.com', '(21) 0000-0000', '(21) 1111-1111', '(21) 2222-2222'), 
+	   ('Cliente 12', 'Rua 12', 4, 'cliente_12@email.com', '(21) 0000-0000', '(21) 1111-1111', '(21) 2222-2222'), 
+	   ('Cliente 13', 'Rua 13', 5, 'cliente_13@email.com', '(21) 0000-0000', '(21) 1111-1111', '(21) 2222-2222'), 
+	   ('Cliente 14', 'Rua 14', 5, 'cliente_14@email.com', '(21) 0000-0000', '(21) 1111-1111', NULL), 
+	   ('Cliente 15', 'Rua 15', 5, 'cliente_15@email.com', '(21) 0000-0000', '(21) 1111-1111', NULL), 
+	   ('Cliente 16', 'Rua 16', 6, 'cliente_16@email.com', '(21) 0000-0000', '(21) 1111-1111', '(21) 2222-2222'), 
+	   ('Cliente 17', 'Rua 17', 6, 'cliente_17@email.com', '(21) 0000-0000', NULL,             NULL), 
+	   ('Cliente 18', 'Rua 18', 6, 'cliente_18@email.com', '(21) 0000-0000', '(21) 1111-1111', '(21) 2222-2222'), 
+	   ('Cliente 19', 'Rua 19', 7, 'cliente_19@email.com', '(31) 0000-0000', '(31) 1111-1111', '(31) 2222-2222'), 
+	   ('Cliente 20', 'Rua 20', 7, 'cliente_20@email.com', '(31) 0000-0000', '(31) 1111-1111', '(31) 2222-2222'), 
+	   ('Cliente 21', 'Rua 21', 7, 'cliente_21@email.com', '(31) 0000-0000', '(31) 1111-1111', '(31) 2222-2222'), 
+	   ('Cliente 22', 'Rua 22', 8, 'cliente_22@email.com', '(31) 0000-0000', '(31) 1111-1111', '(31) 2222-2222'), 
+	   ('Cliente 23', 'Rua 23', 8, 'cliente_23@email.com', '(31) 0000-0000', '(31) 1111-1111', '(31) 2222-2222'), 
+	   ('Cliente 24', 'Rua 24', 8, 'cliente_24@email.com', '(31) 0000-0000', '(31) 1111-1111', '(31) 2222-2222'), 
+	   ('Cliente 25', 'Rua 25', 9, 'cliente_25@email.com', '(31) 0000-0000', NULL,             NULL), 
+	   ('Cliente 26', 'Rua 26', 9, 'cliente_26@email.com', '(31) 0000-0000', '(31) 1111-1111', '(31) 2222-2222'), 
+	   ('Cliente 27', 'Rua 27', 9, 'cliente_27@email.com', '(31) 0000-0000', '(31) 1111-1111', NULL) 
+
+/*
+Criacao de uma tabela para cadastro simples de produtos
+*/
+
+CREATE TABLE dbo.Produto 
+(
+	Id SMALLINT IDENTITY(1, 1) NOT NULL, 
+	Descricao VARCHAR(250) NOT NULL, 
+	CONSTRAINT PK_Produto PRIMARY KEY (Id) 
+) 
+
+/*
+Criacao de um indice auxiliar, para filtragem a partir da coluna Descricao da tabela Produto
+*/
+
+CREATE NONCLUSTERED INDEX IDX_Produto_Descricao ON dbo.Produto (Descricao) 
+
+INSERT INTO dbo.Produto (Descricao) 
+VALUES ('Produto A'), 
+       ('Produto B'), 
+       ('Produto C')
+
+/*
+Criacao de uma tabela de vendas que ira unir informacoes de clientes e produtos
+*/
+
+CREATE TABLE dbo.Vendas 
+(
+	Id BIGINT IDENTITY(1, 1) NOT NULL, 
+	Pedido UNIQUEIDENTIFIER NOT NULL, 
+	Id_Cliente INTEGER NOT NULL, 
+	Id_Produto SMALLINT NOT NULL, 
+	Quantidade SMALLINT NOT NULL, 
+	"Valor Unitario" NUMERIC(9, 2) NOT NULL, 
+	"Data Venda" SMALLDATETIME NOT NULL, 
+	Observacao NVARCHAR(100) NULL, 
+	CONSTRAINT PK_Vendas PRIMARY KEY (Id, Id_Cliente, Id_Produto), 
+	CONSTRAINT UC_Vendas_Pedido_Cliente_Produto UNIQUE (Pedido, Id_Cliente, Id_Produto), 
+	CONSTRAINT FK_CadastroCliente_Vendas FOREIGN KEY (Id_Cliente) REFERENCES CadastroCliente (Id), 
+	CONSTRAINT FK_Produto_Vendas FOREIGN KEY (Id_Produto) REFERENCES Produto (Id) 
+) 
+
+/*
+Criacao de um indice auxiliar, para uso no JOIN atraves das colunas Id_Cliente (com a tabela CadastroCliente) e Id_Produto (com a tabela Produto) 
+*/
+
+CREATE NONCLUSTERED INDEX IDX_Vendas_Id_Cliente ON dbo.Vendas (Id_Cliente) 
+CREATE NONCLUSTERED INDEX IDX_Vendas_Id_Produto ON dbo.Vendas (Id_Produto) 
+
+/*
+Criacao de um indice auxiliar, para filtragem a partir da coluna "Data Venda" da tabela Vendas
+*/
+
+CREATE NONCLUSTERED INDEX IDX_Vendas_DataVenda ON dbo.Vendas("Data Venda") INCLUDE (Quantidade, "Valor Unitario") 
+GO 
+
+CREATE PROCEDURE dbo.PopularVendas 
+AS 
+BEGIN 
+	DECLARE @DataInicial SMALLDATETIME = CAST('1990-01-01' AS SMALLDATETIME) 
+	DECLARE @DataFinal SMALLDATETIME = CAST('2024-03-01' AS SMALLDATETIME) 
+	DECLARE @DataAtual SMALLDATETIME = @DataInicial
+	DECLARE @Bloco SMALLINT = 5000 
+	DECLARE @BlocoAtual SMALLINT = 0 
+	DECLARE @Pedido UNIQUEIDENTIFIER 
+
+	BEGIN TRANSACTION 
+
+	WHILE (@DataFinal > @DataAtual) 
+	BEGIN 
+		IF (@BlocoAtual >= @Bloco) 
+		BEGIN 
+			COMMIT TRANSACTION 
+			BEGIN TRANSACTION 
+			SET @BlocoAtual = 0 
+		END 
+
+		SET @Pedido = NEWID() 
+
+		INSERT INTO dbo.Vendas (Pedido, Id_Cliente, Id_Produto, Quantidade, "Valor Unitario", "Data Venda") 
+		VALUES (@Pedido, 1, 1, 10, 5.65, @DataAtual), 
+			   (@Pedido, 1, 2, 10, 7.65, @DataAtual)
+				
+		SET @Pedido = NEWID() 
+
+		INSERT INTO dbo.Vendas (Pedido, Id_Cliente, Id_Produto, Quantidade, "Valor Unitario", "Data Venda") 
+		VALUES (@Pedido, 2, 1, 20, 5.65, @DataAtual), 
+			   (@Pedido, 2, 2, 20, 7.65, @DataAtual) 
+		
+		SET @Pedido = NEWID() 
+
+		INSERT INTO dbo.Vendas (Pedido, Id_Cliente, Id_Produto, Quantidade, "Valor Unitario", "Data Venda") 
+		VALUES (@Pedido, 3, 1, 30, 5.65, @DataAtual) 
+
+		SET @Pedido = NEWID() 
+
+		INSERT INTO dbo.Vendas (Pedido, Id_Cliente, Id_Produto, Quantidade, "Valor Unitario", "Data Venda") 
+		VALUES (@Pedido, 4, 2, 40, 7.65, @DataAtual) 
+
+		SET @Pedido = NEWID() 
+
+		INSERT INTO dbo.Vendas (Pedido, Id_Cliente, Id_Produto, Quantidade, "Valor Unitario", "Data Venda") 
+		VALUES (@Pedido, 5, 1, 50, 5.65, @DataAtual), 
+			   (@Pedido, 5, 2, 50, 7.65, @DataAtual) 
+	
+		SET @Pedido = NEWID() 
+
+		INSERT INTO dbo.Vendas (Pedido, Id_Cliente, Id_Produto, Quantidade, "Valor Unitario", "Data Venda") 
+		VALUES (@Pedido, 6, 2, 60, 7.65, @DataAtual) 
+
+		SET @Pedido = NEWID() 
+
+		INSERT INTO dbo.Vendas (Pedido, Id_Cliente, Id_Produto, Quantidade, "Valor Unitario", "Data Venda") 
+		VALUES (@Pedido, 7, 1, 70, 5.65, @DataAtual) 
+
+		SET @DataAtual = DATEADD(d, 1, @DataAtual)
+		SET @BlocoAtual = @BlocoAtual + 10 
+	END 
+
+	IF (@BlocoAtual > 0) 
+	BEGIN 
+		COMMIT TRANSACTION 
+	END 
+END 
+GO 
+
+EXEC dbo.PopularVendas 
+GO 
+
+-- ***************************************************************
+-- ***************************************************************
+-- ***************************************************************
+-- ***************************************************************
